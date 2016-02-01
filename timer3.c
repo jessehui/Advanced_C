6432b74d1a79d9f1d336ccd5d6a08ee62dfd1eb2
Jesse <MacBook@10-1-205-12.vlan882.client.wireless.uic.edu>
2016-02-01 09:57:00 -0600

    timer3

--- timer3.c
+++ timer3.c
@@ -14,5 +14,35 @@ void ledinit()
 
 void T3init()
 {
-	T3CTL |= 0x08;//1111 1000
+	//系统在不配置工作频率时默认为 2分频，即 32M/2=16M，所以定时
+	//器每次溢出时 T=1/(16M/128)*65536≈0.5s, 所以总时间
+	//Ta=T*count=0.5*1=0.5S切换 1次状态
+	T3CTL |= 0x08;//0000 1000 开溢出中断
+	T3IE = 1;//开启T3
+	T3CTL |= 0xE0; //1110 0000 128分频 128/1600 0000 *N = 0.5s, N = 62500
+	T3CTL &= ~0x03;//0000 0011 计数模式是从0x00-0xff 共255个数 625000/255 = 245
+	T3CTL |= 0x10;//0001 0000 定时器开始启动
+	EA = 1;//开启总中断
+
 }
+
+//定时器T3中断服务函数
+#pragma vector = T3_VECTOR
+__interrupt void T3_ISR(void)
+{
+	IRCON = 0x00;//先清零
+	if(count++ > 245)
+		{
+			count = 0;
+			LED1 = ~LED1;
+		}
+}
+
+void main(void)
+{
+	ledinit();
+	T3init();
+	While(1);
+}
+
+
