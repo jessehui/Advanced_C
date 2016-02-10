dc4cb2abeba52768db9548e76e8a7922722a38d9
Jesse <scalpel.etf@gmail.com>
2016-02-10 11:16:51 -0600

    task creating test

---
+++ uCOS-II/main(task).c
@@ -0,0 +1,61 @@
+#include "includes.h"
+
+//****************START 任务
+#define START_TASK_PRIO		10//开始任务的优先级设计为最低
+//任务堆栈
+#define START_STK_SIZE 		64
+OS_STK START_TASK_STK[START_STK_SIZE];//OS_STK类型数组
+
+//任务函数声明
+void start_task(void *pdata);
+
+
+//****************LED0任务
+#define LED0_TASK_PRIO		7
+//led0Task任务堆栈
+#define LED0_STK_SIZE 		64
+OS_STK LED0_TASK_STK[LED0_STK_SIZE];
+//任务函数
+void led0Task(void *pdata);
+
+void Delay(unsigned int t)
+{
+	while(t--);
+}
+
+int main(void)
+{
+	OSInit();
+	OSTaskCreate(start_task, 
+				(void *)0, //空指针 即NULL
+				(OS_STK *)START_TASK_STK[START_STK_SIZE - 1],//最后一个元素作为栈顶
+				START_TASK_PRIO);//创建起始任务
+}
+
+void start_task(void *pdata)
+{
+	OS_CPU_SR cpu_sr = 0;
+	pdata = pdata;
+
+	delay_init(168);
+
+	OS_ENTER_CRITICLE();	//进入临界区 无法被中断打断
+	OSTaskCreate(led0Task, 
+				(void *)0, 
+				(OS_STK *)LED0_TASK_STK[LED0_STK_SIZE-1],
+				LED0_TASK_PRIO);
+	OS_EXIT_CRITICLE();		//退出临界区 可以被中断打断
+
+}
+
+void led0Task(void *pdata)
+{
+	LED_Init();
+	while(1)
+	{
+		OPEN_LED0();
+		Delay(100000);
+		CLOSE_LED0();
+		Delay(100000);
+	}
+}
