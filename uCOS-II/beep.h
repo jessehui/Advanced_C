e94bb22118fc136e7c388c073c6522c3924fc6ef
Jesse Chunyang Hui <scalpel.etf@gmail.com>
2016-02-10 11:39:54 -0600

    indent change

--- uCOS-II/beep.h
+++ uCOS-II/beep.h
@@ -2,7 +2,7 @@
 #define __BEEP_H
 
 //beep端口定义 高电平工作 低电平关闭
-#define OPEN_BEEP()		GPIO_SetBits(GPIOF,GPIO_Pin_8)
+#define OPEN_BEEP() 	GPIO_SetBits(GPIOF,GPIO_Pin_8)
 #define CLOSE_BEEP()	GPIO_ResetBits(GPIOF,GPIO_Pin_8)
 
 void BEEP_Init(void);
