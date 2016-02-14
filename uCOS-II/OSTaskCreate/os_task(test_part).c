a3c358ee4acbc6367dbe16df1f009fc322db3d97
Jesse <scalpel.etf@gmail.com>
2016-02-14 17:45:17 -0600

    merge os_task.c

--- uCOS-II/OSTaskCreate/os_task(test_part).c
+++
@@ -1,6 +0,0 @@
-INT8U OSTaskCreate( 	void (*task)(void *p_arg),//函数指针 task是函数地址，*p_arg为参数。
-			//整个参数的意义是指向任务起始位置的指针
-			void *p_arg,//传递给任务的参数 一般为NULL
-			OS_STK *ptos,//指向任务堆栈栈顶的指针。OS_STK是任务堆栈的数据类型
-			INT8U prio//任务优先级
-		 )
