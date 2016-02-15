5251167d23e3612817f09de0026609df68361b4b
Jesse <scalpel.etf@gmail.com>
2016-02-15 11:03:55 -0600

    task delete, name set and get, task suspend

--- uCOS-II/os_task(test_part).c
+++ uCOS-II/os_task(test_part).c
@@ -37,3 +37,63 @@ typedef struct os_stk_data{
 INT8U OSTaskStkChk(	INT8U prio,
 			OS_STK_DATA *p_stk_data
 )
+
+// //**************************************************
+// 			OS TASK DELETE
+// Auguments: prio 	-- priority of the task to delete. Note that you can
+// 			 explicitly delete the current task without knowing the priority
+// 			 level by setting prio to OS_PRIO_SELF
+#if OS_TASK_DEL_EN > 0u
+INT8U OSTaskDel(INT8U prio)
+{
+
+}
+
+// *******************************************************
+// 		Task Delete Request
+// Arguments:  prio 	
+// Return: 
+// 	OS_ERR_NONE			no error.task existed and request registered
+// 	OS_ERR_TASK_NOT_EXIST  		Task has been deleted.
+// 	OS_ERR_TASK_DEL			if the task is assigned to a mutex(互斥体)
+// 	OS_ERR_TASK_DEL_IDLE		if you requested to delete uCOS's idle task
+// 	OS_ERR_PRIO_INVALID		invalid priority
+// 	OS_ERR_TASK_DEL_REQ		if a task request that the running task be deleted
+#if OS_TASK_DEL_EN > 0u
+INT8U OSTaskDelReq(INT8U prio)
+
+// *******************************************************
+// 		Task Name set
+// Arguments: perr -- a pointer to an error code that contains one of the following values
+		
+// 		OS_ERR_NONE 		if the requested task is resumed
+// 		OS_ERR_TASK_NOT_EXIST	if task is not created or assigned to a mutex
+// 		OS_ERR_TASK_NAME_TOO_LONG 
+// 		OS_ERR_PNAME_NULL	passed a wrong pointer to pname
+// 		OS_ERR_PRIO_INVALID	
+// 		OS_ERR_NAME_SET_ISR	If you called this function from an ISR(interrupt service request)
+
+
+#if OS_TASK_NAME_EN > 0u
+void OSTaskNameSet(INT8U prio,
+		   INT8U *pname,//指向任务名字的指针
+		   INT8U *perr)//错误码指针
+
+// *******************************************************
+// 		Task Name get
+//Return: the length of the name string or 0 if the task doesn't exist
+#if OS_TASK_NAME_EN > 0u
+INT8U OSTaskNameGet(INT8U prio,
+		    INT8U **pname,//双指针 指向指针的指针
+		    INT8U *perr）//错误码指针
+
+// *******************************************************
+// 		Task Suspend
+//Returns:
+		 // OS_ERR_NONE
+		 // OS_ERR_TASK_SUSPEND_IDLE  you can't suspend an idle task
+		 // OS_ERR_PRIO_INVALID
+		 // OS_ERR_TASK_SUSPEND_PRIO  if the task to suspend doesn't exist
+		 // OS_ERR_TASK_NOT_EXIST	   if the task is assigned to a mutex 
+#if OS_TASK_SUSPEND_EN > 0u
+INT8U OSTaskSuspend(INT8U prio)		    
