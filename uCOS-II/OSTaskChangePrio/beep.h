977fd92a44cdaa29a588ce1451e29263587fa6c4
Jesse <scalpel.etf@gmail.com>
2016-02-16 15:03:25 -0600

    clean repeated files

--- uCOS-II/OSTaskChangePrio/beep.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BEEP_H
-#define __BEEP_H
-
-//beep端口定义 高电平工作 低电平关闭
-#define OPEN_BEEP() 	GPIO_SetBits(GPIOF,GPIO_Pin_8)
-#define CLOSE_BEEP()	GPIO_ResetBits(GPIOF,GPIO_Pin_8)
-
-void BEEP_Init(void);
-#endif
